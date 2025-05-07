97ade38289cd52b099fdcc13e7bf1aba42e278df
savbace <7579898+savbace@users.noreply.github.com>
2025-05-07 15:01:55 +0300

    do add proxy for prod build

--- sportsexplorer.client/vite.config.ts
+++ sportsexplorer.client/vite.config.ts
@@ -1,57 +1,65 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
-import child_process from 'child_process';
-import { env } from 'process';
+import { defineConfig } from "vite";
+import plugin from "@vitejs/plugin-react";
+import fs from "fs";
+import path from "path";
+import child_process from "child_process";
+import { env } from "process";
 
 const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASP.NET/https`
-        : `${env.HOME}/.aspnet/https`;
+  env.APPDATA !== undefined && env.APPDATA !== "" ? `${env.APPDATA}/ASP.NET/https` : `${env.HOME}/.aspnet/https`;
 
 const certificateName = "sportsexplorer.client";
 const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
 if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
+  if (
+    0 !==
+    child_process.spawnSync(
+      "dotnet",
+      ["dev-certs", "https", "--export-path", certFilePath, "--format", "Pem", "--no-password"],
+      { stdio: "inherit" }
+    ).status
+  ) {
+    throw new Error("Could not create certificate.");
+  }
 }
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7125';
+const target = env.ASPNETCORE_HTTPS_PORT
+  ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
+  : env.ASPNETCORE_URLS
+  ? env.ASPNETCORE_URLS.split(";")[0]
+  : "https://localhost:7125";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => {
+  if (command === "build") {
+    return {
+      plugins: [plugin()],
+    };
+  }
+
+  return {
     plugins: [plugin()],
     resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
     server: {
-        proxy: {
-            '^/api': {
-                target,
-                secure: false
-            }
+      proxy: {
+        "^/api": {
+          target,
+          secure: false,
         },
-        port: 5173,
-        https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
-        }
-    }
-})
+      },
+      port: 5173,
+      https: {
+        key: fs.readFileSync(keyFilePath),
+        cert: fs.readFileSync(certFilePath),
+      },
+    },
+  };
+});
